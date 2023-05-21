b55439ce94ef6038c3c78a2b8181599d45911ce4
Michael Simm <michael@simm.dev>
2023-05-21 20:49:44 +0100

    Add logo to header

--- src/Heading.js
+++ src/Heading.js
@@ -1,5 +1,11 @@
 import React from "react";
+import HotelIcon from "./img/castle.svg"
 
-const Heading = () => <header className="App-header">CYF Hotel</header>;
+const Heading = () => (
+  <header className="App-header">
+    <img src={HotelIcon} className="invert" alt="Hotel Logo"/>
+    CYF Hotel
+  </header>
+);
 
 export default Heading;
