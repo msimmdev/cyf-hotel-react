d761612886bb014922a8d07d75a42d6410788c75
Michael Simm <michael@simm.dev>
2023-05-21 19:35:37 +0100

    Reformat

--- src/Heading.js
+++ src/Heading.js
@@ -1,7 +1,5 @@
 import React from "react";
 
-const Heading = () => {
-    return <header className="App-header">CYF Hotel</header>;
-}
+const Heading = () => <header className="App-header">CYF Hotel</header>;
 
 export default Heading;
--- src/SearchButton.js
+++ src/SearchButton.js
@@ -1,7 +1,7 @@
 import React from "react";
 
-const SearchButton = (props) => {
-    return <button className="btn btn-primary">{props.children}</button>;
-};
+const SearchButton = (props) => (
+  <button className="btn btn-primary">{props.children}</button>
+);
 
 export default SearchButton;
